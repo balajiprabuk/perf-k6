92f18ee70e0eb788b35eb27275cc8f62a671428e
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-19 20:10:21 +0530

    Update checks.js

--- src/checks.js
+++ src/checks.js
@@ -1,4 +1,5 @@
 import http from "k6/http";
+import faker from '../../perf-k6/modules/faker.js'; 
 import { SharedArray } from "k6/data";
 import { check } from "k6";
 //importing checks from k6
@@ -10,13 +11,17 @@ const data = new SharedArray("user details for registration", function () {
 export default function () {
   const url = "https://test-api.k6.io/user/register/";
   const params = { headers: { "Content-Type": "application/json" } };
-  const payload = JSON.stringify(data[__ITER]);
-  let response = http.post(url, payload, params);
-  
+  const payload = {
+    username: faker.internet.userName(),
+    first_name: faker.name.firstName(),
+    last_name: faker.name.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+  let response = http.post(url,JSON.stringify(payload),params)
   check(response, {
     "response code was 201": (res) => response.status == 201,
   });
-  
   /* checks are similar to assertion but does not stop the execution. Stores the result based on condition
      let the script execution continue*/
 }

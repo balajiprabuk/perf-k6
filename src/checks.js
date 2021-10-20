f019e022a2b20fa0a04a5886ec31b1c5e8e2acb1
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-20 17:08:41 +0530

    Update checks.js

--- src/checks.js
+++ src/checks.js
@@ -1,24 +1,21 @@
 import http from "k6/http";
 import faker from '../../perf-k6/modules/faker.js'; 
-import { SharedArray } from "k6/data";
 import { check } from "k6";
 //importing checks from k6
 
-const data = new SharedArray("user details for registration", function () {
-  return JSON.parse(open("../../perf-k6/data/parameterizedData.json")).users;
-});
+const BASE_URL = "https://test-api.k6.io";
 
 export default function () {
-  const url = "https://test-api.k6.io/user/register/";
-  const params = { headers: { "Content-Type": "application/json" } };
-  const payload = {
+  const URL = `${BASE_URL}/user/register/`;
+  const PARAMS = { headers: { "Content-Type": "application/json" } };
+  const PAYLOAD = {
     username: faker.internet.userName(),
     first_name: faker.name.firstName(),
     last_name: faker.name.firstName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
-  let response = http.post(url,JSON.stringify(payload),params)
+  let response = http.post(URL,JSON.stringify(PAYLOAD),PARAMS)
   check(response, {
     "response code was 201": (res) => response.status == 201,
   });

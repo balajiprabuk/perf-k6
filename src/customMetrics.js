31aff3bf9be1b8c6cc64aab8deae45793460ee6c
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-20 17:19:54 +0530

    Update customMetrics.js

--- src/customMetrics.js
+++ src/customMetrics.js
@@ -1,15 +1,12 @@
 import http from "k6/http";
 import faker from "../modules/faker.js";
-import { SharedArray } from "k6/data";
-import { group } from "k6";
+import { check, group } from "k6";
 import { Trend } from "k6/metrics";
+//importing trend metrics from k6
 
 let registerTrendObject = new Trend("http_req_duration_registration");
 let loginTrendObject = new Trend("http_req_duration_login");
-
-const data = new SharedArray("user details for registration", function () {
-  return JSON.parse(open("../../perf-k6/data/parameterizedData.json")).users;
-});
+const BASE_URL = "https://test-api.k6.io";
 
 export let options = {
   thresholds: {
@@ -19,31 +16,38 @@ export let options = {
 };
 
 export default function () {
-  let username, password;
+  let username = faker.internet.userName();
+  let password = faker.internet.password();
   group("User registration", function () {
-    username = faker.internet.userName();
-    password = faker.internet.password();
-    const url = "https://test-api.k6.io/user/register/";
-    const params = { headers: { "Content-Type": "application/json" } };
-    const payload = {
+    const URL = `${BASE_URL}/user/register/`;
+    const PARAMS = { headers: { "Content-Type": "application/json" } };
+    const PAYLOAD = {
       username: username,
       password: password,
       first_name: faker.name.firstName(),
       last_name: faker.name.firstName(),
       email: faker.internet.email(),
     };
-    let response = http.post(url, JSON.stringify(payload), params);
+    let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
     registerTrendObject.add(response.timings.duration);
+
+    check(response, {
+      "User registration response code should be 201": (res) =>
+        response.status == 201,
+    });
   });
 
   group("Login User", function () {
-    const url = "https://test-api.k6.io/auth/basic/login/";
-    const payload = {
+    const URL = `${BASE_URL}/auth/basic/login/`;
+    const PAYLOAD = {
       username: username,
       password: password,
     };
-    const params = { headers: { "Content-Type": "application/json" } };
-    let response = http.post(url, JSON.stringify(payload), params);
+    const PARAMS = { headers: { "Content-Type": "application/json" } };
+    let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
     loginTrendObject.add(response.timings.duration);
+    check(response, {
+      "login code should be 200": (res) => response.status == 200,
+    });
   });
 }

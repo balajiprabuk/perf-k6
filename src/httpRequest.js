21ebd5565c48309be8f5764f6f99685563637b2a
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-20 16:51:47 +0530

    Update httpRequest.js

--- src/httpRequest.js
+++ src/httpRequest.js
@@ -1,25 +1,26 @@
 import http from "k6/http";
 // importing http module that contains functionality for performing HTTP transactions
 
+const BASE_URL = "https://test-api.k6.io";
 
-//k6 entry point 
+//k6 entry point
 export default function () {
-  const url = "https://test-api.k6.io/user/register/";
-  const payload = {
-    username: "sample1111",
-    first_name: "test",
-    last_name: "user",
-    email: "test11111@tester.com",
-    password: "tester",
+  const URL = `${BASE_URL}/user/register/`;
+  const PAYLOAD = {
+    username: "demo",
+    first_name: "demo",
+    last_name: "demo",
+    email: "demo@example.com",
+    password: "demo",
   };
-  const params = { headers: { "Content-Type": "application/json" } };
+  const PARAMS = { headers: { "Content-Type": "application/json" } };
 
   //url type type string
   //payload type string / object / ArrayBuffer.objects will be x-www-form-urlencoded
   //params type object
-  let response = http.post(url, JSON.stringify(payload), params);
-  // returns a Response object 
-  console.log(response.status)
+  let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
+  // returns a Response object
+  console.log("response code is :", response.status);
 }
 
 // k6 run httpRequest.js || k6 run httpRequest.js --iterations=6 --vus=2

0e8d299aed75c3dbc73e34c31d89aeaf0859effa
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-20 17:04:28 +0530

    Update requestParameterization.js

--- src/requestParameterization.js
+++ src/requestParameterization.js
@@ -2,17 +2,19 @@ import http from "k6/http";
 import { SharedArray } from "k6/data";
 //importing sharedArray from k6
 
-const data = new SharedArray("user details for registration", function() { return JSON.parse(open('../../perf-k6/data/parameterizedData.json')).users; });
+const PAYLOAD_DATA = new SharedArray("user details for registration", function() { return JSON.parse(open('../../perf-k6/data/parameterizedData.json')).users; });
 // array-like object that shares the underlying memory between VUs
 
+const BASE_URL = "https://test-api.k6.io";
+
 export default function () {
-  const url = "https://test-api.k6.io/user/register/";
-  const params = { headers: { "Content-Type": "application/json" } };
-  const payload = JSON.stringify(data[__ITER])
+  const URL = `${BASE_URL}/user/register/`;
+  const PARAMS = { headers: { "Content-Type": "application/json" } };
+  const PAYLOAD = JSON.stringify(PAYLOAD_DATA[__ITER])
   //__ITER is predefined k6 environment variable which stores the iteration index 
 
-  let response = http.post(url, payload, params);
-  console.log(response.status)
+  let response = http.post(URL, PAYLOAD, PARAMS);
+  console.log("response code is :", response.status);
 }
 
 

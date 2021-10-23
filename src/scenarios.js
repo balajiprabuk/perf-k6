213fee8229ece6aa63339970d4b1d1c72cc15818
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-23 11:11:20 +0530

    Update scenarios.js

--- src/scenarios.js
+++ src/scenarios.js
@@ -1,17 +1,18 @@
-import http from "k6/http";
-import faker from "../modules/faker.js";
-import { check, group } from "k6";
-import { Trend } from "k6/metrics";
+import http from 'k6/http';
+import faker from '../modules/faker.js';
+import { check, group } from 'k6';
+import { Trend } from 'k6/metrics';
 
-const BASE_URL = "https://test-api.k6.io";
-let loginCrocodileTrend = new Trend("http_req_duration_login_crocodile");
+const BASE_URL = 'https://test-api.k6.io';
+let loginCrocodileTrend = new Trend('http_req_duration_login_crocodile');
+
 export let options = {
   scenarios: {
     login_crocodile_shared: {
-      executor: "shared-iterations",
+      executor: 'shared-iterations',
       vus: 2,
       iterations: 10,
-      maxDuration: "10s",
+      maxDuration: '10s',
     },
     login_crocodile_ramp: {
       executor: 'ramping-vus',
@@ -24,50 +25,28 @@ export let options = {
     },
   },
 };
-
 export function setup() {
-  let username = faker.internet.userName();
-  let password = faker.internet.password();
-  let authToken;
-  group("User registration", function () {
-    const URL = `${BASE_URL}/user/register/`;
-    const PARAMS = { headers: { "Content-Type": "application/json" } };
-    const PAYLOAD = {
-      username: username,
-      password: password,
-      first_name: faker.name.firstName(),
-      last_name: faker.name.firstName(),
-      email: faker.internet.email(),
-    };
-    let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
-    check(response, {
-      "User registration response code should be 201": (res) =>
-        response.status == 201,
-    });
-  });
+  const registeredUserName = 'vod';
+  const registeredPassword = 'vod';
 
-  group("Login User", function () {
-    const URL = `${BASE_URL}/auth/token/login/`;
-    const PAYLOAD = {
-      username: username,
-      password: password,
-    };
-    let response = http.post(URL, JSON.stringify(PAYLOAD), {
-      headers: { "Content-Type": "application/json" },
-    });
-    authToken = JSON.parse(response.body).access;
-    check(authToken, { "logged in successfully": () => authToken !== "" });
+  const URL = `${BASE_URL}/auth/token/login/`;
+  const PAYLOAD = {
+    username: registeredUserName,
+    password: registeredPassword,
+  };
+  let response = http.post(URL, JSON.stringify(PAYLOAD), {
+    headers: { 'Content-Type': 'application/json' },
   });
+  const authToken = JSON.parse(response.body).access;
   return authToken;
 }
-
 export default function (authToken) {
-  group("Create a new crocodile", function () {
+  group('Create a new crocodile', function () {
     const URL = `${BASE_URL}/my/crocodiles/`;
     const PAYLOAD = {
       name: faker.name.findName(),
-      sex: "M",
-      date_of_birth: "2001-01-01",
+      sex: 'M',
+      date_of_birth: '2001-01-01',
     };
     const PARAMS = {
       headers: {
@@ -75,11 +54,13 @@ export default function (authToken) {
       },
     };
     let response = http.post(URL, PAYLOAD, PARAMS);
-    loginCrocodileTrend.add(response.timings.duration)
-    check(response, { "Created crocodile successfully": () => response.status == 201 });
+    loginCrocodileTrend.add(response.timings.duration);
+    check(response, {
+      'Created crocodile successfully': () => response.status == 201,
+    });
   });
 }
 
 export function teardown(authToken) {
-  group("Perform resource cleanups steps", function () {});
+  //Perform resource cleanups steps
 }

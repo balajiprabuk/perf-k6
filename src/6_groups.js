215c827fa015b69667f501d66908e26c58fe4a02
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-23 11:10:36 +0530

    Update and rename tags_groups.js to 6_groups.js

--- src/6_groups.js
+++ src/6_groups.js
@@ -1,23 +1,24 @@
-import http from "k6/http";
-import faker from "../modules/faker.js";
-import { check, group } from "k6";
+import http from 'k6/http';
+import faker from '../modules/faker.js';
+import { check, group } from 'k6';
 //importing group from k6
 
-const BASE_URL = "https://test-api.k6.io";
+const BASE_URL = 'https://test-api.k6.io';
 
 export let options = {
   thresholds: {
-    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
+    'checks{Tag:LoginUser}': ['rate>0.9'],
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95% of requests should be below 200ms
   },
 };
 
 export default function () {
   let username = faker.internet.userName();
   let password = faker.internet.password();
-  group("User registration", function () {
+  group('User registration', function () {
     const URL = `${BASE_URL}/user/register/`;
-    const PARAMS = { headers: { "Content-Type": "application/json" } };
+    const PARAMS = { headers: { 'Content-Type': 'application/json' } };
     const PAYLOAD = {
       username: username,
       password: password,
@@ -27,21 +28,25 @@ export default function () {
     };
     let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
     check(response, {
-      "User registration response code should be 201": (res) =>
+      'User registration response code should be 201': (res) =>
         response.status == 201,
     });
   });
 
-  group("Login User", function () {
+  group('Login User', function () {
     const URL = `${BASE_URL}/auth/basic/login/`;
     const PAYLOAD = {
       username: username,
       password: password,
     };
-    const PARAMS = { headers: { "Content-Type": "application/json" } };
+    const PARAMS = { headers: { 'Content-Type': 'application/json' } };
     let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
-    check(response, {
-      "login code should be 200": (res) => response.status == 200,
-    });
+    check(
+      response,
+      {
+        'login code should be 200': (res) => response.status == 200,
+      },
+      { Tag: 'LoginUser' }
+    );
   });
 }

b87d080012f93369108469cc9fc21ddcb3e5df7b
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-23 11:10:04 +0530

    Update and rename checks.js to 4_checks.js

--- src/4_checks.js
+++ src/4_checks.js
@@ -1,13 +1,13 @@
-import http from "k6/http";
-import faker from '../../perf-k6/modules/faker.js'; 
-import { check } from "k6";
+import http from 'k6/http';
+import faker from '../modules/faker.js'; 
+import { check } from 'k6';
 //importing checks from k6
 
 const BASE_URL = "https://test-api.k6.io";
 
 export default function () {
   const URL = `${BASE_URL}/user/register/`;
-  const PARAMS = { headers: { "Content-Type": "application/json" } };
+  const PARAMS = { headers: { 'Content-Type': 'application/json' } };
   const PAYLOAD = {
     username: faker.internet.userName(),
     first_name: faker.name.firstName(),
@@ -17,7 +17,7 @@ export default function () {
   };
   let response = http.post(URL,JSON.stringify(PAYLOAD),PARAMS)
   check(response, {
-    "response code was 201": (res) => response.status == 201,
+    'response code was 201': (res) => response.status == 201,
   });
   /* checks are similar to assertion but does not stop the execution. Stores the result based on condition
      let the script execution continue*/

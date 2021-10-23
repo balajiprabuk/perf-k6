ad03a4e6e4c05178546c817da5b874a488b25e17
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-23 09:51:14 +0530

    Update testlifeCycle.js

--- src/testlifeCycle.js
+++ src/testlifeCycle.js
@@ -3,43 +3,31 @@ import faker from "../modules/faker.js";
 import { check, group } from "k6";
 import { Trend } from "k6/metrics";
 
-const BASE_URL = "https://test-api.k6.io";
 let loginCrocodileTrend = new Trend("http_req_duration_login_crocodile");
-export let options = {};
+
+const BASE_URL = "https://test-api.k6.io";
+
+export let options = {
+  thresholds: {
+    http_req_duration_login_crocodile: ["p(95)<400"], // 95% of requests should be below 400ms
+  },
+};
 
 export function setup() {
-  let username = faker.internet.userName();
-  let password = faker.internet.password();
-  let authToken;
-  group("User registration", function () {
-    const URL = `${BASE_URL}/user/register/`;
-    const PARAMS = { headers: { "Content-Type": "application/json" } };
-    const PAYLOAD = {
-      username: username,
-      password: password,
-      first_name: faker.name.firstName(),
-      last_name: faker.name.firstName(),
-      email: faker.internet.email(),
-    };
-    let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
-    check(response, {
-      "User registration response code should be 201": (res) =>
-        response.status == 201,
-    });
-  });
 
-  group("Login User", function () {
-    const URL = `${BASE_URL}/auth/token/login/`;
-    const PAYLOAD = {
-      username: username,
-      password: password,
-    };
-    let response = http.post(URL, JSON.stringify(PAYLOAD), {
-      headers: { "Content-Type": "application/json" },
-    });
-    authToken = JSON.parse(response.body).access;
-    check(authToken, { "logged in successfully": () => authToken !== "" });
+  const registeredUserName = "";
+  const registeredPassword = "";
+
+  const URL = `${BASE_URL}/auth/token/login/`;
+  const PAYLOAD = {
+    username: registeredUserName,
+    password: registeredPassword,
+  };
+  let response = http.post(URL, JSON.stringify(PAYLOAD), {
+    headers: { "Content-Type": "application/json" },
   });
+  const authToken = JSON.parse(response.body).access;
+  console.log(authToken)
   return authToken;
 }
 
@@ -57,11 +45,13 @@ export default function (authToken) {
       },
     };
     let response = http.post(URL, PAYLOAD, PARAMS);
-    loginCrocodileTrend.add(response.timings.duration)
-    check(response, { "Created crocodile successfully": () => response.status == 201 });
+    loginCrocodileTrend.add(response.timings.duration);
+    check(response, {
+      "Created crocodile successfully": () => response.status == 201,
+    });
   });
 }
 
-export function teardown(authToken) {
-  group("Perform resource cleanups steps", function () {});
+export function teardown() {
+  //Perform resource cleanups steps
 }

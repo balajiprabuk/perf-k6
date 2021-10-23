340b24e265ba98715caa18914c45ac63f3c9f062
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-23 11:09:48 +0530

    Update and rename dynamicData.js to 3_dynamicData.js

--- src/3_dynamicData.js
+++ src/3_dynamicData.js
@@ -1,13 +1,13 @@
-import http from "k6/http";
-import faker from '../../perf-k6/modules/faker.js'; 
+import http from 'k6/http';
+import faker from '../modules/faker.js'; 
 //importing faker library using browserify. 
 //with this we can import npm modules as internal module 
 
-const BASE_URL = "https://test-api.k6.io";
+const BASE_URL = 'https://test-api.k6.io';
 
 export default function () {
   const URL = `${BASE_URL}/user/register/`;
-  const PARAMS = { headers: { "Content-Type": "application/json" } };
+  const PARAMS = { headers: { 'Content-Type': 'application/json' } };
   
   //Using faker library generating random data at run time for all the iterations.This ensures dynamic data passed
   //at run time for our payload 
@@ -22,7 +22,7 @@ export default function () {
 
 
   let response = http.post(URL,JSON.stringify(PAYLOAD),PARAMS)
-  console.log("response code is :", response.status)
+  console.log('response code is :', response.status)
   //All repsonse are 201 because of unique data passed in payload
 }
 

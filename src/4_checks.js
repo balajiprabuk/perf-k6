995e3e13020cd666d40edb7d0dfda573144dfe80
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-10-29 09:56:08 +0530

    Update 4_checks.js

--- src/4_checks.js
+++ src/4_checks.js
@@ -1,13 +1,15 @@
-import http from 'k6/http';
-import faker from '../modules/faker.js'; 
-import { check } from 'k6';
+import http from "k6/http";
+import faker from "../modules/faker.js";
+import { check } from "k6";
 //importing checks from k6
+/* checks are similar to assertion but does not stop the execution. Stores the result based on condition
+     let the script execution continue*/
 
 const BASE_URL = "https://test-api.k6.io";
 
 export default function () {
   const URL = `${BASE_URL}/user/register/`;
-  const PARAMS = { headers: { 'Content-Type': 'application/json' } };
+  const PARAMS = { headers: { "Content-Type": "application/json" } };
   const PAYLOAD = {
     username: faker.internet.userName(),
     first_name: faker.name.firstName(),
@@ -15,10 +17,13 @@ export default function () {
     email: faker.internet.email(),
     password: faker.internet.password(),
   };
-  let response = http.post(URL,JSON.stringify(PAYLOAD),PARAMS)
+  let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
   check(response, {
-    'response code was 201': (res) => response.status == 201,
+    "response code was 201": (res) => res.status === 201,
   });
-  /* checks are similar to assertion but does not stop the execution. Stores the result based on condition
-     let the script execution continue*/
+  //value - Value to test
+  //sets - assertion condition to perform against value
+  //tags - tags to attach in metrics
+
+  // k6 run --vus 1 --iterations 3  4_checks.js
 }

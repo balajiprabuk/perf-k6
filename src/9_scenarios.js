e9de6f73f83ec9e6578b03ca5c236037f580099a
kowcikr <80941920+kowcikr@users.noreply.github.com>
2021-11-12 21:20:41 +0530

    Update 9_scenarios.js

--- src/9_scenarios.js
+++ src/9_scenarios.js
@@ -6,38 +6,37 @@ const BASE_URL = "https://test-api.k6.io";
 
 export let options = {
   thresholds: {
-    "checks{Tag:fixed_iterations}": ["rate>0.9"],
-    "checks{Tag:shared_iterations}": ["rate>0.9"],
-    "http_req_duration{test_type:shared}": ["p(95)<900"],
-    "http_req_duration{test_type:fixed}": ["p(99)<900"],
+    "checks{pattern:Load}": ["rate>0.9"],
+    "checks{pattern:Spike}": ["rate>0.9"],
+    "http_req_duration{pattern:Spike": ["p(95)<900"],
+    "http_req_duration{pattern:Load": ["p(95)<900"],
   },
   //Scenarios allow us to make in depth configurations to how VUs and iterations are scheduled
   scenarios: {
     login_crocodile_shared: {
-      executor: "shared-iterations",
-      //Deliver the expected load pattern
-      vus: 2,
-      iterations: 6,
-      maxDuration: "10s",
-      env: {
-        loadType: "shared_iterations",
-      },
+      executor: "constant-arrival-rate",
+      rate: 5,
+      duration: "0m10s",
+      preAllocatedVUs: 5,
+      maxVUs: 10,
+      tags: { pattern: "Load" },
     },
     login_crocodile_fixed: {
-      executor: "per-vu-iterations",
+      executor: "ramping-vus",
       startTime: "11s",
-      vus: 2,
-      iterations: 4,
-      env: {
-        loadType: "fixed_iterations",
-      },
+      startVUs: 1,
+      stages: [
+        { duration: "5s", target: 10 },
+        { duration: "5s", target: 0 },
+      ],
+      tags: { pattern: "Spike" },
     },
   },
 };
 
 export default function () {
-  const registeredUserName = "vdemo";
-  const registeredPassword = "vdemo";
+  const registeredUserName = "catchup";
+  const registeredPassword = "catchup";
 
   const URL = `${BASE_URL}/auth/token/login/`;
   const PAYLOAD = {
@@ -62,14 +61,10 @@ export default function () {
       },
     };
     let response = http.post(URL, PAYLOAD, PARAMS);
-    check(
-      response,
-      {
-        "Created crocodile successfully": () => response.status == 201,
-      },
-      { tag: __ENV.loadType }
-    );
+    check(response, {
+      "Created crocodile successfully": () => response.status == 201,
+    });
   });
 }
 
-//k6 run scenarios.js
+//k6 run 9_scenarios.js

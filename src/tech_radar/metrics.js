7cb5b53e54ceefadb31cea6a99bbbd676d9693d3
Kowcik R <kowcik.r@thoughtworks.com>
2023-01-19 08:39:32 +0530

    added threshold

--- src/tech_radar/metrics.js
+++ src/tech_radar/metrics.js
@@ -1,18 +1,24 @@
 import http from "k6/http";
 import faker from "../../modules/faker.js";
 import { check } from "k6";
+import { Trend } from 'k6/metrics';
 
 const BASE_URL = "https://test-api.k6.io";
+
+/*Counters - sum values.
+Trends - calculate statistics for multiple values (like mean or mode).
+Rates - track how frequently a non-zero value occurs.
+Gauges - track the smallest, largest, and latest values */
+
+const myTrend = new Trend('response_time');
 
 export const options = {
   vus: 10,
   iterations: 10,
-/*  thresholds: {
-    // the rate of successful checks should be higher than 99%
-    checks: ["rate>0.99"],
-    http_req_failed: ["rate<0.01"], // http errors should be less than 1%
+  thresholds: {
+    checks: ["rate>0.99"],     // the rate of successful checks should be higher than 99%
     http_req_duration: ["p(95)<700"], // 95% of requests should be below 700ms
-  }*/
+  }
 };
 export default function () {
   const URL = `${BASE_URL}/user/register/`;
@@ -25,6 +31,9 @@ export default function () {
     password: faker.internet.password(),
   };
   let response = http.post(URL, JSON.stringify(PAYLOAD), PARAMS);
+  
+   myTrend.add(response.timings.duration);
+ 
   check(response, {
     "response code was 201": (res) => response.status == 201,
   });
